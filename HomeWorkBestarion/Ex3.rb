2a8b84bcb5a130bfa0f5d82e69661683e23d575f
ZeroToH3ro <mtblaser2002@gmail.com>
2022-09-29 22:41:56 +0700

    done ex3

--- HomeWorkBestarion/Ex3.rb
+++ HomeWorkBestarion/Ex3.rb
@@ -1,34 +1,36 @@
-def find_ascending_array
-  number = 0;
+CONST_MAX = 99
+CONST_MIN = 0
+
+def find_ascending_array  
+  number = 0
   loop do
-  print "Enter the number of element in array: ";
-  number = gets.to_i;
-  break if number <= 99 and number > 0
-  print "Value > 99 or < 0, please enter again: ";
+    print "Enter the number of element in array: "
+    number = gets.to_i
+    break if number <= CONST_MAX and CONST_MIN < number
+    print "Value > 99 or < 0, please enter again: \n"
   end
-  
-  new_array = Array.new();
-  $count = 1;
-  array_random = Array.new(number){rand(0..99)};
-  print array_random;
+
+  new_array = Array.new()
+  $count = 1
+  array_random = Array.new(number) { rand(1..9) }
+  print array_random
 
   array_random.each_with_index do |element, index|
-    if !array_random[index+1].nil? && array_random[index] <= array_random[index+1]
-      new_array.append(element);
+    if !array_random[index + 1].nil? && array_random[index] <= array_random[index + 1]
+      new_array.append(element)
     else
-      if index == array_random.length-1 && array_random[index-1] < array_random[index] 
-      then
-        new_array.append(element).to_s;
-        print "\nSub array #{$count}: ", new_array;
-        break;
+      if index == array_random.length - 1 && array_random[index - 1] < array_random[index]
+        new_array.append(element).to_s
+        print "\nSub array #{$count}: ", new_array
+        break
       end
-      new_array.append(element).to_s;
-      print "\nSub array #{$count}: ", new_array;
-      new_array.clear;
-      $count += 1;
+      new_array.append(element).to_s
+      print "\nSub array #{$count}: ", new_array
+      new_array.clear
+      $count += 1
     end
   end
 end
 
-find_ascending_array;
-puts;
+find_ascending_array
+puts
